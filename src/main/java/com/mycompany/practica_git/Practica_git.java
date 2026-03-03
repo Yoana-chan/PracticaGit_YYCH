0644cc8152ce47913087cf06025963b5f3a43eb2
Pomuch8 <Pomuch8@DESKTOP-21HCBL5>
2026-03-03 09:22:36 -0600

    feat:estructura inicial del proyecto

---
+++ src/main/java/com/mycompany/practica_git/Practica_git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica_git;
+
+/**
+ *
+ * @author Pomuch8
+ */
+public class Practica_git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
