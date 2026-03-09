e2be999a1f424e9c9cff3493bc6f413c29fae4cd
Pomuch8 <Pomuch8@DESKTOP-21HCBL5>
2026-03-09 08:04:10 -0600

    feat:estructura inicial del proyecto

--- src/main/java/com/mycompany/practica_git/Practica_git.java
+++ src/main/java/com/mycompany/practica_git/Practica_git.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.practica_git;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Pomuch8
@@ -13,8 +15,19 @@ public class Practica_git {
     public static void main(String[] args) {
         System.out.println("sistema de gestion");
         System.out.println("Bienvenido,usuario");
-        
-      
+        Scanner cin = new Scanner(System.in);
+
+int n1, n2, suma ;
+
+System.out.print("Introduce el primer número: ");
+n1 = cin.nextInt();
+
+System.out.print("Introduce el segundo número: ");
+  n2 = cin.nextInt();
+  
+  suma = n1 + n2;
+
+System.out.println("el resultado es " + suma );
        
     
         
